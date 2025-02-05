2bf8e0f33caaabfa4aa0c42255449965b36d36e3
Sovex05 <sdaum486@gmail.com>
2025-02-05 18:53:06 +0100

    Initial commit

---
+++ src/main/java/net/sovex/luminite/SovexsLuminiteClient.java
@@ -0,0 +1,10 @@
+package net.sovex.luminite;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SovexsLuminiteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sovex/luminite/SovexsLuminiteDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sovex.luminite;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SovexsLuminiteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
